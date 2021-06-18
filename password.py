50d35dd0db1146b19a0a9faf72ab87826fcc9ab1
Kun Yi <kunyi1989@gmail.com>
2021-06-18 15:29:19 +0800

    更新

--- password.py
+++ password.py
@@ -3,11 +3,13 @@ fre = 3
 fre = int(fre)
 
 while fre > 0:
+	fre = fre - 1
 	uesr = input('請輸入密碼: ')
 	if uesr == password :
 		print('登入成功')
 		break
 	elif uesr != password : 
-		fre = fre - 1
-		print('密碼錯誤! 還有', fre, '次機會!')
+		print('密碼錯誤!')
+		if fre > 0:
+			print('還有', fre, '次機會!')
 		
